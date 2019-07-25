74f5d8b576f2ceda63b4d794ece678b1843f3a80
sh1ft <pmayzenberg@gmail.com>
2019-07-25 15:06:33 +0300

    w

---
+++ HomeWork1 - Ex2/src/PerimeterMeasurable.java
@@ -0,0 +1,4 @@
+
+public interface PerimeterMeasurable {
+	double getPerimeter();
+}
---
+++ HomeWork1 - Ex2/src/PlaneShape.java
@@ -0,0 +1,16 @@
+
+public class PlaneShape extends Shape implements PerimeterMeasurable, AreaMeasurable {
+
+	@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getPerimeter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
